677c34d0d9df1a51d7fc804ba6421a38d143647e
injoyai <1113655791@qq.com>
2025-08-13 09:24:55 +0800

    增加日志实例

--- frame.go
+++ frame.go
@@ -1,5 +1,10 @@
 package frame
 
+import (
+	"fmt"
+	"time"
+)
+
 const (
 	DefaultPort = 8080
 )
@@ -7,4 +12,21 @@ const (
 type Logger interface {
 	Printf(format string, v ...any) (int, error)
 	Println(v ...any) (int, error)
+}
+
+var _ Logger = (*log)(nil)
+
+func NewLogger() Logger {
+	return &log{}
+}
+
+type log struct{}
+
+func (this *log) Printf(format string, v ...any) (int, error) {
+	return fmt.Printf("\x1b[36m"+time.Now().Format(time.TimeOnly)+" "+format+"\x1b[0m", v...)
+}
+
+func (this *log) Println(v ...any) (int, error) {
+	s := fmt.Sprint(v...)
+	return fmt.Println("\x1b[36m", time.Now().Format(time.TimeOnly), " ", s, "\x1b[0m")
 }
